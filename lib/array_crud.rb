6b1c3cf72738a8d75e558dfa0cda3752d218f45a
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-01 14:35:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["wow", "I", "am", "really"]
+  my_array = ["I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
