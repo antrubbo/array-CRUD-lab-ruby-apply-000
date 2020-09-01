32ea480545d5b805124a9a71742cedc55ce4c4b5
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-01 14:32:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array.unshift("wow")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  my_array.pop()
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  my_array.shift()
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_array[1]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
